a48b36a1dc801f647f849773de6864142b836fab
orhns <orhns@ORHAN>
2024-03-31 16:03:47 +0300

    Add project files.

---
+++ MyPortfolio/Controllers/AboutController.cs
@@ -0,0 +1,42 @@
+﻿using MyPortfolio.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyPortfolio.Controllers
+{
+    public class AboutController : Controller
+    {
+        MyAcademyPortfolioProjectEntities db = new MyAcademyPortfolioProjectEntities();
+        public ActionResult Index()
+        {
+            var values = db.TblAbouts.ToList();
+            return View(values);
+        }
+
+        [HttpGet]
+        public ActionResult AddAbout()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult AddAbout(TblAbouts abouts)
+        {
+            db.TblAbouts.Add(abouts);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult DeleteAbout(int id)
+        {
+            var about =  db.TblAbouts.Find(id);
+            db.TblAbouts.Remove(about);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+ 
+    }
+}
